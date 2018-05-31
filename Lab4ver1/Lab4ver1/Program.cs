82d03349a721f2fb0175e8c13c4bb1ffe7494921
matvey1985 <lisakf@mail.ru>
2018-05-31 09:07:32 +0400

    First commit

---
+++ Lab4ver1/Lab4ver1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4ver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Swapper change = new Swapper();
+            int[] array = { 1, 35, 185, 4, 250, 82 };
+            (int, int, int[]) result = change.SwapMaxMax(array);
+            Console.WriteLine(result.Item1);
+            Console.WriteLine(result.Item2);
+            for (int i = 0; i < result.Item3.Length; i++)
+            {
+                Console.WriteLine(result.Item3[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab4ver1/Lab4ver1/Swapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab4ver1
+{
+    public class Swapper
+    {
+        public (int, int, int[]) SwapMaxMax(int[] array)
+        {
+            int max = array[0];
+            int maxindex = 0;
+            for (int k = 0; k < array.Length; k++)
+            {
+                if (max < array[k])
+                {
+                    max = array[k];
+                    maxindex = k;
+                }
+            }
+            int nearMax1 = 0;
+            int nearMax2 = 1;
+            int summoftwo1 = max - (array[0] + array[1]);
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == maxindex) continue;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (j == maxindex) continue;
+                    int sumoftwobuffer = max - (array[i] + array[j]);
+                    if (sumoftwobuffer < summoftwo1)
+                    {
+                        nearMax1 = i;
+                        nearMax2 = j;
+                        summoftwo1 = sumoftwobuffer;
+                    }
+                }
+            }
+            int buffer = array[nearMax1];
+            array[nearMax1] = array[nearMax2];
+            array[nearMax2] = buffer;
+
+            return (array[nearMax1], array[nearMax2], array);
+        }
+    }
+}
