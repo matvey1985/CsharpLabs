4354c5567820ef8b36a20d42d6ec9381bd5612a7
matvey1985 <lisakf@mail.ru>
2018-06-04 04:07:35 +0400

    The second approved lab

---
+++ Lab2ver1/Lab2ver1/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Lab2ver1
+{
+    class Counter
+    {
+        public int CountSymbolsA(string text)
+        {
+            int internalcount = 0;
+            char[] separators = new char[] { ' ', '.' };
+            string[] words = text.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+            bool[] rightACount = new bool[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                internalcount = 0;
+                internalcount = words[i].ToCharArray().Count(c=>((c=='a')|(c=='а')));
+                if (internalcount == 3)
+                {
+                    rightACount[i] = true;
+                }
+                else { rightACount[i] = false; }
+            }
+            return rightACount.Count(e => e==true);
+        }
+    }
+}
---
+++ Lab2ver1/Lab2ver1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2ver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Counter CountA = new Counter();
+            Console.WriteLine("Введите предложение для подсчета букв !");
+            String buffer = Console.ReadLine();
+            Console.WriteLine(CountA.CountSymbolsA(buffer));
+            Console.ReadLine();
+        }
+    }
+}
