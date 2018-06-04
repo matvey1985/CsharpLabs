e8fbde8aa91fa1d784aad6a9e274138f1208936b
matvey1985 <lisakf@mail.ru>
2018-06-04 04:44:00 +0400

    Lab 7 approved

---
+++ Lab7ver1/TPO lab7withArrays/ColumnSwaper.cs
@@ -0,0 +1,29 @@
+﻿namespace TPO_lab7withArrays
+{
+    class ColumnSwaper
+    {
+        public int[,] swapColumns(int[,] array)
+        {
+            int counter = 0;
+            int indexOfSecondOdd = 0;
+            int indexOfLastNegative = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                if (array[0, i] % 2 == 0) { counter = counter + 1; }
+                if (counter == 2) { indexOfSecondOdd = i; break; }
+            }
+            
+            for (int i = array.GetLength(1) - 1; i >= 0; i--)
+            {
+                if (array[0, i] < 0) { indexOfLastNegative = i; break; }
+            }
+            int[,] clone = (int[,])array.Clone();
+            for (int j = 0; j < clone.GetLength(0); j++)
+            {
+                array[j, indexOfSecondOdd] = clone[j, indexOfLastNegative];
+                array[j, indexOfLastNegative] = clone[j, indexOfSecondOdd];
+            }
+            return array;
+        }
+    }
+}
---
+++ Lab7ver1/TPO lab7withArrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TPO_lab7withArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColumnSwaper swaper = new ColumnSwaper();
+            int[,] inputArray = new int[6, 9];
+            Random rnd = new Random();
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    inputArray[i, j] = rnd.Next(-99, 99);
+                }
+            }
+
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    Console.Write(inputArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            swaper.swapColumns(inputArray);
+            Console.WriteLine("\n");
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    Console.Write(inputArray[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+       
+    }
+}
