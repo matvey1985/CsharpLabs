42e8db43deb8c3e98738b32191827db67823e63c
matvey1985 <lisakf@mail.ru>
2018-06-04 05:13:25 +0400

    Lab 6 approved

---
+++ lab6ver1/lab6ver1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6ver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Remover remover = new Remover();
+            List<int> randomList = new List<int>() { 1, 2, 3,12, 16, 8, 24, 10, 18, 4 };
+            remover.removeSomeElements(randomList);
+            foreach(int x in randomList)
+            {
+                Console.WriteLine(x.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lab6ver1/lab6ver1/Remover.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6ver1
+{
+    public class Remover
+    {
+        public List<int> removeSomeElements(List<int> array)
+        {
+            int firstElement = array[0];
+            int lastElement = array[array.Count-1];
+            int moduloRest;
+            int difference = lastElement - firstElement;
+           
+            for(int i=array.Count-1;i>=0;i--)
+            {
+                moduloRest = array[i] % difference;
+                if (moduloRest==0)
+                {
+                    array.RemoveAt(i);
+                }
+            }
+            return array;
+        }
+    }
+}
