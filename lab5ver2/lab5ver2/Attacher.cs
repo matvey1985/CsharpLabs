f3cf9f5190216831491e46106cf2d5eba84544fb
matvey1985 <lisakf@mail.ru>
2018-06-04 04:15:04 +0400

    The approved and corrected lab for C#

---
+++ lab5ver2/lab5ver2/Attacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab5ver2
+{
+    class Attacher
+    {
+        public List<int> AttachNewElements(List<int> array)
+        {
+            int maximum = array.Max();
+            int minimum = array.Min();
+            int difference = maximum - minimum;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if ((array[i] % 2 == 0) && (array[i] < 0))
+                {
+                    array.Insert(i + 1, difference);
+                    i++;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ lab5ver2/lab5ver2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab5ver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Attacher attacher = new Attacher();
+            List<int> numbers = new List<int>() {-12, -3, -4, 5, 3, 4, 5, -12, -4, -5, 6, 5, 4 };
+            attacher.AttachNewElements(numbers);
+
+            foreach(int x in numbers)
+            {
+                Console.WriteLine(x.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
