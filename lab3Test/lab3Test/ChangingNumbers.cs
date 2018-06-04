db5ca04fcd7afd1bb5c530704c7c0dd6f809145c
matvey1985 <lisakf@mail.ru>
2018-06-04 04:35:31 +0400

    Lab 3 approved

---
+++ lab3Test/lab3Test/ChangingNumbers.cs
@@ -0,0 +1,17 @@
+﻿namespace lab3Test
+{
+    class ChangingNumbers
+    {
+        public int CheckChange(int[] array) {
+            for (int i = 1; i < array.Length; i++) {
+                int previousModula = array[i - 1] % 2;
+                int currentModula = array[i] % 2;
+                if ((previousModula == currentModula)|(previousModula == -currentModula))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ lab3Test/lab3Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab3Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChangingNumbers Chered = new ChangingNumbers();
+            int[] massiv= { 1, 2, 3, 4, 5, -65 };
+
+            if (Chered.CheckChange(massiv)==-1) {
+                Console.WriteLine(0); }
+            else {
+               Console.WriteLine("Числа не чередуются начиная с элемента номер " + (Chered.CheckChange(massiv))); }
+            Console.ReadLine();
+        }
+    }
+}
