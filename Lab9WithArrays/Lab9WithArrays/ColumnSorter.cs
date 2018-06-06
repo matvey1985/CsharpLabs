a4a7671076961af09e95d4d9e7188295a2b4e64c
matvey1985 <lisakf@mail.ru>
2018-06-06 10:36:11 +0400

    Lab9 first version with 2D Arrays not approved yet

---
+++ Lab9WithArrays/Lab9WithArrays/ColumnSorter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9WithArrays
+{
+    class ColumnSorter
+    {
+        public int[,] sortColumns(int[,] array)
+        {
+            int max=0;
+            int indexOfColumnWithMax = 0;
+            int[] bufferarray = new int[array.GetLength(0)];
+            for (int i=0;i<array.GetLength(0);i++)
+            {
+                for (int j = 0; j < array.GetLength(1);j++)
+                {
+                 if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                        indexOfColumnWithMax = j;
+                    }
+                }
+            }
+            Console.WriteLine(indexOfColumnWithMax);
+
+            for (int i = 0; i < bufferarray.Length; i++)
+            {
+                bufferarray[i] = array[i, indexOfColumnWithMax];
+                Console.Write(bufferarray[i]);
+            }
+            Array.Sort(bufferarray);
+            int[,] clone = (int[,])array.Clone();
+
+            for(int k = 0; k < bufferarray.Length; k++)
+            {
+                for (int j =0;j<clone.GetLength(0);j++)
+                {
+                    if (clone[j, indexOfColumnWithMax] == bufferarray[k])
+                    {
+                        for(int l=0;l<array.GetLength(1);l++)
+                        {
+                            array[k, l] = clone[j, l];
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
---
+++ Lab9WithArrays/Lab9WithArrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9WithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColumnSorter sorter = new ColumnSorter();
+            int[,] inputArray = new int[6, 9];
+            Random rnd = new Random();
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    inputArray[i, j] = rnd.Next(-99, 99);
+                }
+            }
+
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    Console.Write(inputArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            sorter.sortColumns(inputArray);
+
+            Console.WriteLine("\n");
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    Console.Write(inputArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+    
