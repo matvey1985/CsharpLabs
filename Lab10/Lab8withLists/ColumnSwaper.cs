52e55865f67732fd8bc0b397f93d5e583f911b6b
matvey1985 <lisakf@mail.ru>
2018-06-06 10:09:10 +0400

    Lab 8 improved and made with List of Lists, approved as Lab10

---
+++ Lab10/Lab8withLists/ColumnSwaper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8withLists
+{
+    class ColumnSwaper
+    {
+        public List<List<int>> swapColums(List<List<int>> array)
+        {
+            List<List<int>> bufferArray = new List<List<int>>();
+            List<int> bufferListWithMax = new List<int>();
+            bufferArray = new List<List<int>>(array);
+            int max = 0;
+            int IndexOfRowWithMax = 0;
+            int n = 0;
+            for(int i = 0; i < bufferArray.Count;i++)
+            {
+                for (int j=0;j<bufferArray[i].Count;j++)
+                {
+                    if (bufferArray[i][j] > max) {
+                        max = bufferArray[i][j];
+                        IndexOfRowWithMax = i;
+                    }
+                }
+                 n = bufferArray.Count;
+            }
+            bufferListWithMax = new List<int>(bufferArray[IndexOfRowWithMax]);
+           
+            for (int i = bufferArray.Count-1; i >=0; i--)
+            {
+                for (int j = 0; j < bufferArray[i].Count; j++)
+                {
+                    if ((bufferArray[i][j]%2!=0)&&(bufferArray[i][j]<0))
+                    {                      
+                        bufferArray.Insert(i+1, bufferListWithMax);
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < bufferArray.Count; i++)
+            {
+                for (int j = 0; j < bufferArray[i].Count; j++)
+                {
+                    Console.Write(" " + bufferArray[i][j]);
+                }
+                Console.WriteLine(" ");
+                
+            }
+            Console.WriteLine(" " + IndexOfRowWithMax);
+            Console.ReadKey();
+            return bufferArray;
+
+        }
+    }
+}
---
+++ Lab10/Lab8withLists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8withLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColumnSwaper swaper = new ColumnSwaper();
+            List<List<int>> newArray = new List<List<int>>();
+            newArray.Add(new List<int> { 2, 3, -3, -4, 5, 6, 7, 8, -10, -2 });
+            newArray.Add(new List<int> { 2, 3, -3, -4, 5, 6, 7, 8, -10, -2 });
+            newArray.Add(new List<int> { 2, 3, -6, -4, 5, 6, 7, 16, -10, -2 });
+            newArray.Add(new List<int> { 2, 3, -17, -4, 5, 25, 7, 8, -10, -2 });
+
+            swaper.swapColums(newArray);
+            var max = newArray.Count;
+        }
+    }
+}
